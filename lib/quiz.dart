d154ad960fb9d41f6efc747c0b3a9e95fdefcb48
choilc <choilv99@gmail.com>
2023-07-31 10:19:29 +0700

    start screen

---
+++ lib/quiz.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:quizz_app/question_screen.dart';
+import 'package:quizz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 34, 3, 87),
+                Color.fromARGB(255, 55, 3, 145),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
